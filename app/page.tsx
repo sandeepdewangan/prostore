0766bb5de98adea6a2fd380667e6c898e3375dc4
Sandeep <sandeepdewangan7@gmail.com>
2025-04-26 12:36:35 +0530

    Shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Prostore</h1>;
+  return (
+    <>
+      <h1>Prostore</h1>
+      <Button>Click Me</Button>
+    </>
+  );
 }
